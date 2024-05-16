c251b54e6ce6882d261690e5163682e5c69f78e7
Pedro <pedrohjb79@gmail.com>
2024-05-16 13:35:44 -0300

    Aula sobre vs code e dotnet

---
+++ DIO-AVANADE/exemplo-fundamentos/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Clube de Regatas Vasco da Gama");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Teste Icons");
---
+++ DIO-AVANADE/exemplo-fundamentos/readme.md
@@ -0,0 +1,6 @@
+## Teste de funcionalidades do VD Code
+
+### Atalhos CRTL + '   =    Abrir Pront de comando 
+### Atalhos CRTL + SHIFT + '   =    Abrir Pront de comando 
+
+### 
